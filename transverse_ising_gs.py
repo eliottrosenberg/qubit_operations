1256972887f9d91c11d65793bc87a9f3d5e80236
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-11-08 21:43:17 -0800

    Add files via upload

--- transverse_ising_gs.py
+++ transverse_ising_gs.py
@@ -13,8 +13,17 @@ def fermion_H(n,g,J=1):
     H = coo_array( (data, (i,j)) )
     return H
 
-def ising_ground_state(n,g,J=1):
+def ising_ground_state_energy(n,g,J=1):
     # returns the ground state energy of the transverse field Ising model
     H = fermion_H(n,g,J).toarray()
     l = svd(H, compute_uv=False)
     return -sum(l)
+
+def ising_ground_state(n,g,J=1):
+    H = fermion_H(n,g,J).toarray()
+    Otilde, l, OT = svd(H)
+    E = -sum(l)
+    M = Otilde @ OT
+    ZZ = np.array([M[i,i+1] for i in range(n-1)])
+    X = -np.diag(M)
+    return E, ZZ, X
